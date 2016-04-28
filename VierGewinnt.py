3b61ee3a46b8e4b8f8c242bd8cc234f9ba4b6842
Constantinos Stergiou <stergiou@cip.ifi.lmu.de>
2016-04-28 20:35:51 +0200

    "

--- VierGewinnt.py
+++ VierGewinnt.py
@@ -2,17 +2,9 @@ board = []
 width = 7
 height = 6
 min_connected = 4
-
-chips = ["X", "O"]
 players = [input("Player "+str(i)+", please enter your name: ") for i in range(2)]
-
-print("Hello",players[0],"and",players[1],", fight!")
-
-# erstellung des spielfeldes, während das board vertikal orientiert ist
-for y in range(height):
-    board.append([])
-    for x in range(width):
-        board[-1].append("_")
+current_player = 0
+chips = ["X", "O"]
 
 def print_board():
     # die erste zeile
@@ -22,15 +14,15 @@ def print_board():
         # join fügt das zwischen den elementen ein
         print("|".join(line))
 
+# vier gewinnt
 def game_ended():
     result = False
-
     # die anzahl der hintereinanderliegenden chips
     connected_count = 0
     connected_player = 0
 
     test_lines = board[0:]
-    # add columns, drehung des boards um 90 grad
+    # add columns, vertauschung der x und y werte
     for x in range(0, width):
         test_lines.append([])
         for y in range(0, height):
@@ -39,39 +31,45 @@ def game_ended():
     # durchlauf der hintereinanderliegenden
     for line in test_lines:
         for chip in line:
+            # in der luft
             if chip == "_":
                 connected_count = 0
                 continue
-
             chip_player = chips.index(chip)
             if connected_count == 0 or connected_player != chip_player:
                 connected_count = 1
                 connected_player = chip_player
             else:
                 connected_count += 1
-
+            # erzielte anzahl an hintereinanderliegenden
             if connected_count == min_connected:
                 result = True
                 break
-
         if result:
             break
-
+        
     if result:
         print(players[connected_player]+", YOU WON!")
-
     return result
 
-current_player = 0
+print("Hello",players[0],"and",players[1],", fight!")
 
+# erstellung des spielfeldes, während das board vertikal orientiert ist
+for y in range(height):
+    board.append([])
+    for x in range(width):
+        board[-1].append("_")
+
+# selektion der spalte
 while not game_ended():
     print_board()
     print("Player",players[current_player],", your move!")
+
     col = -1
     while col < 0:
         try:
             col = int(input("Enter column: "))
-            print(board[0][col])
+            print(board[0][col-1])
             # ob die spalte innerhalb der breite sich befindet
             if col not in range(1, width+1):
                 col = -1
@@ -82,11 +80,13 @@ while not game_ended():
         except ValueError:
             continue
     col -= 1
+
     row = 0
     # solange der boden nicht erreicht wird und er sich in der luft befindet
     while (row+1) < height and board[row+1][col] == "_":
         row += 1
-    # der zelle wird der zugehörige chip zugeordnet
+
+    # der chip des zugehörigen spielers ist gefallen
     board[row][col] = chips[current_player]
     # alternation zwischen 0 und 1
     current_player = 1 - current_player
