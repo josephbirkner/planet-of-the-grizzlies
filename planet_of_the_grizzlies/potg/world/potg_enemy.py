38652fa1b5b687a0376c69ace37fac1db5419a57
stergiou <con.stergiou@hotmail.com>
2016-06-15 19:09:20 +0200

    Added PatrollingEnemy

--- planet_of_the_grizzlies/potg/world/potg_enemy.py
+++ planet_of_the_grizzlies/potg/world/potg_enemy.py
@@ -1,13 +1,14 @@
 
 from potg_entity import *
+from potg_functions import sgn
 
 
 class Enemy(Entity):
 
     size = [139/1.5, 177/1.5, 10]
 
-    def __init__(self, pos, world):
-        super().__init__(pos, world, "gfx/stripper.png")
+    def __init__(self, pos, world, image):
+        super().__init__(pos, world, image)
 
     def collision(self, colliding_entity):
         if colliding_entity.entity_type() == "P":
@@ -15,3 +16,26 @@ class Enemy(Entity):
 
     def entity_type(self):
         return "E"
+
+
+class PatrollingEnemy(Entity):
+
+    speed = 8
+    direction = 0
+
+    def __init__(self, pos, world, direction):
+        super().__init__(pos, world, "gfx/bart.png")
+        self.direction = direction
+        self.velocity[direction] = self.speed
+
+    def update(self):
+        platform_box = self.platform.box
+        distances = []
+        for axis in range(0,3):
+            distances.append([self.box.position[axis]-platform_box.position[axis], platform_box.position[axis]+platform_box.size[axis]-self.box.position[axis]-self.box.size[axis]])
+        if distances[self.direction][1-sgn(self.velocity[self.direction])]<abs(self.velocity[self.direction]):
+            self.velocity[self.direction] *= -1
+        super(PatrollingEnemy,self).update()
+
+    def entity_type(self):
+        return "F"
